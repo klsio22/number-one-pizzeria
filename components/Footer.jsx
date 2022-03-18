3fed9de7efe57a70f28e1a233743303e6d47270d
Klesio <klesio.dev@gmail.com>
2022-03-18 02:29:40 -0300

    Finished footer page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import styles from '../styles/Footer.module.scss';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image
+          src='/img/bg.png'
+          layout='fill'
+          alt='glass of wine in the table'
+        />
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.numberOne}>
+            OH YES , WE DID .THE LAMA PIZZA , WELL BEKED SLICE OF PIZZA
+          </h2>
+        </div>
+
+        <div className={styles.card}>
+          <h1 className={styles.title}>Find our Restaurants</h1>
+          <p className={styles.text}>
+            1654.Ron Read #304
+            <br /> NewYork , 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className={styles.text}>
+            1654.Ron Read #304
+            <br /> NewYork , 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className={styles.text}>
+            1654.Ron Read #304
+            <br /> NewYork , 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className={styles.text}>
+            1654.Ron Read #304
+            <br /> NewYork , 85022
+            <br /> (602) 867-1010
+          </p>
+        </div>
+
+        <div className={styles.card}>
+          <h1 className={styles.title}>Working Hours</h1>
+          <p className={styles.text}>
+            Monday until Friday
+            <br /> 9:00 - 22:00
+          </p>
+          <p className={styles.text}>
+            Monday until Friday
+            <br /> 9:00 - 22:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
