5b179b582046dff8ce9695da8904dc6d95b38ba7
Klesio <klesio.dev@gmail.com>
2022-03-17 23:49:01 -0300

    Added new modified in files

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Featured from '../components/Featured';
-//import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 
 // eslint-disable-next-line
 import "swiper/css/bundle";
+
+import PizzzList from '../components/PizzaList';
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Featured/>
-      homepage
+      <PizzzList/>
     </div>
   );
 }
